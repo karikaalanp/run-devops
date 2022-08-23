483c6702ccc972203f5aabe2ede8b5d70f0521f5
Karikalan P <karikaalanp@outlook.com>
2022-08-23 13:17:57 +0530

    initial push

---
+++ src/Shopping/Shopping.Client/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Client.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping_Client.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(ProductContext.Products);
+        }
+    }
+}
